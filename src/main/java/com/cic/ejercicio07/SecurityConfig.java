b110a73841814856c6dae8e39885c00a4e6396cb
curso14 <curso14@nomail.com>
2024-08-12 09:25:25 +0200

    Test ok

--- src/main/java/com/cic/ejercicio07/SecurityConfig.java
+++ src/main/java/com/cic/ejercicio07/SecurityConfig.java
@@ -5,10 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer.ExpressionInterceptUrlRegistry;
-import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -17,22 +14,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-   /*  @SuppressWarnings("removal")
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(authorizeRequests ->
-                authorizeRequests
-                    .requestMatchers("/public").permitAll()
-                    .requestMatchers("/coches").authenticated()
-                    .requestMatchers("/coches/**").authenticated()
-                    .anyRequest().permitAll()
-            )
-            .oauth2ResourceServer(oauth2ResourceServer ->
-            oauth2ResourceServer.jwt()
-            );
-        return http.build();
-    }*/
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
